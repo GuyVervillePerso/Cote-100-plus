ba7de194d341e1f1864a85e4b6646fb4ae564131
Guy Verville <vervilleguy@icloud.com>
2024-07-23 15:13:19 -0400

    Login

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 // import vue2 from '@vitejs/plugin-vue2';
-import * as Sentry from "@sentry/browser";
-
-Sentry.init({
-    dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
-});
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/site.css',
-                'resources/js/site.js',
+                "resources/css/site.css",
+                "resources/js/site.js",
 
                 // Control Panel assets.
                 // https://statamic.dev/extending/control-panel#adding-css-and-js-assets
                 // 'resources/css/cp.css',
                 // 'resources/js/cp.js',
             ],
-            refresh: [
-                'resources/views/**',
-            ],
+            refresh: ["resources/views/**"],
         }),
         // vue2(),
     ],
