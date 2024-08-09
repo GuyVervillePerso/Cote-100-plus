fd873dbdd7019a7bb8d5beddda0183804d26905d
Guy Verville <vervilleguy@proton.me>
2024-08-09 10:32:41 -0400

    Création de caches

--- app/Listeners/EntrySavedListener.php
+++ app/Listeners/EntrySavedListener.php
@@ -15,5 +15,20 @@ class EntrySavedListener
         if ($event->entry->collectionHandle() === 'titres') {
             Cache::tags(['portfolios'])->flush();
         }
+        if ($event->entry->collectionHandle() === 'entre_les_lignes') {
+            Cache::tags(['entre_les_lignes'])->flush();
+            Cache::forget('btl_topsection_default');
+            Cache::forget('btl_topsection_anglais');
+        }
+        if ($event->entry->collectionHandle() === 'billets_mensuels') {
+            Cache::tags(['billets_mensuels'])->flush();
+            Cache::forget('bm_topsection_default');
+            Cache::forget('bm_topsection_anglais');
+        }
+        if ($event->entry->collectionHandle() === 'commentaires_trimestriels') {
+            Cache::tags(['commentaires_trimestriels'])->flush();
+            Cache::forget('mc_topsection_default');
+            Cache::forget('mc_topsection_anglais');
+        }
     }
 }
